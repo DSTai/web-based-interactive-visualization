3d90391abd557b9ba673f8cf00aa451081b08b41
DSTai <itdsiu20132@student.hcmiu.edu.vn>
2024-05-24 23:04:20 +0700

    add new function
    add select region function, legend for regions,  highlight provinces in range bar, zoom to region bounding box

--- geomap.js
+++ geomap.js
@@ -47,12 +47,11 @@ var colorScales = {
     grdp: d3.scaleThreshold().range(colorSchemes.grdp)
 };
 
-// Function to create legend
 function createLegend(color, thresholds) {
     const legendWidth = 300;
     const legendHeight = 20;
     const legendMargin = { top: 50, right: 20, bottom: 20, left: 20 };
-
+    legendThresholds = thresholds;
     const legend = svg.append("g")
         .attr("class", "legend")
         .attr("transform", `translate(${legendMargin.left},${legendMargin.top})`);
@@ -73,7 +72,41 @@ function createLegend(color, thresholds) {
         .attr("y", 0)
         .attr("width", (d, i) => legendScale(thresholds[i + 1] || thresholds[thresholds.length - 1]) - legendScale(d))
         .attr("height", legendHeight)
-        .attr("fill", (d, i) => color(d));
+        .attr("fill", (d, i) => color(d))
+        .on("mouseover", function(event, d) {
+            // Highlight selected legend rectangle
+            d3.select(this)
+                .attr("stroke", "black")
+                .attr("stroke-width", 1);    
+                        
+            var className = ".legend-" + thresholds.indexOf(d); // Get class based on threshold
+            svg.selectAll(".province")
+                .classed("highlighted", false); // Reset all provinces
+            svg.selectAll(".province")
+                .filter(function(province) {
+                    var value = province.properties.value;
+                    var minThreshold = d;
+                    var maxThreshold = thresholds[thresholds.indexOf(d) + 1] || thresholds[thresholds.length - 1];
+                    return value >= minThreshold && value <= maxThreshold;
+                })
+                .classed("highlighted", true)
+                .style("fill", "yellow"); // Highlight provinces based on class
+        })
+        .on("mouseout", function() {
+            legend.selectAll("rect")
+            .attr("stroke", "none");
+
+            svg.selectAll(".province")
+                .classed("highlighted", false)
+                .style("fill", function(d) {
+                    var value = d.properties.value;
+                    if (value) {
+                        return color(value);
+                    } else {
+                        return "#ccc";
+                    }
+                });
+        });
 
     legend.append("g")
         .attr("transform", `translate(0,${legendHeight})`)
@@ -115,7 +148,7 @@ function loadData() {
         });
         var min = d3.min(values);
         var max = d3.max(values);
-        var thresholds = d3.range(min, max, (max - min) / (color.range().length));
+        var thresholds = d3.range(min, max + (max - min) / (color.range().length), (max - min) / (color.range().length));
         color.domain(thresholds);  
         
         d3.json("https://dstai.github.io/data/vn-provinces.json").then(function(json) {
@@ -238,6 +271,115 @@ function loadData() {
         svg.selectAll(".legend").remove();
         createLegend(color, thresholds);
 
+ 
+// Load region data and create legend
+d3.csv("https://dstai.github.io/data/region.csv").then(function(regionData) {
+    // Extract region names from regionData
+    var uniqueRegions = Array.from(new Set(regionData.map(region => region.region)));
+
+    // Create legend using region names
+    createRegionLegend(uniqueRegions);
+
+    // Merge the region data with GeoJSON
+    regionData.forEach(function(region) {
+        var regionCode = region.code; // Assuming 'code' is the region code in regionData
+        var regionName = region.region; // Assuming 'region' is the region name in regionData
+
+        // Find the corresponding feature in GeoJSON and assign the region information
+        json.features.forEach(function(feature) {
+            if (parseFloat(feature.properties.Ma) === parseFloat(regionCode)) {
+                feature.properties.region = regionName;
+            }
+        });
+    });
+
+    // Proceed with your existing code for map rendering
+}).catch(function(error) {
+    console.error("Error loading region data:", error);
+});
+    function createRegionLegend(regions) {
+        const legendWidth = 30;
+        const legendHeight = 30;
+        const legendMargin = { top: 50, right: 20, bottom: 20, left: 20 };
+        var regionColors = {
+            "Dong bang song Hong": "red",
+            "Trung du va mien nui phia Bac": "darkgreen",
+            "Bac Trung Bo va Duyen hai mien Trung": "navy",
+            "Tay Nguyen": "darkmagenta",
+            "Dong Nam Bo": "goldenrod",
+            "Dong bang song Cuu Long": "darkcyan"
+        };
+        
+        const regionLegend = svg.append("g")
+            .attr("class", "region-legend")
+            .attr("transform", `translate(${legendMargin.left},${legendMargin.top + 100})`);
+        
+        regionLegend.selectAll("rect")
+            .data(regions)
+            .enter()
+            .append("rect")
+            .attr("x", 0)
+            .attr("y", (d, i) => i * (legendHeight + 5))
+            .attr("width", legendWidth)
+            .attr("height", legendHeight)
+            .attr("fill", d => regionColors[d] )
+            .attr("stroke", "black")
+            .attr("stroke-width", 0.2)
+            .on("mouseover", function(event, d) {
+                var regionName = d;
+            
+                // Fill all provinces with grey except for the selected region
+                svg.selectAll(".province")
+                    .style("fill", function(province) {
+                        return province.properties.region === regionName ? color(province.properties.value) : "#ccc";
+                    })
+                    .classed("highlighted", function(province) {
+                        return province.properties.region === regionName;
+                    })
+                    .style("stroke", function(province) {
+                        return province.properties.region === regionName ? regionColors[regionName]: null;
+                    })
+                    .attr("stroke-width", function(province) {
+                        return province.properties.region === regionName ? 0.2 : 0;
+                    });
+            
+                // Optionally, you can zoom to the bounding box of the highlighted region
+                zoomToRegion(regionName, json);
+            })
+            .on("mouseout", function() {
+                // Reset province highlighting and color
+                svg.selectAll(".province")
+                    .classed("highlighted", false)
+                    .style("fill", function(d) {
+                        var value = d.properties.value;
+                        if (value) {
+                            return color(value);
+                        } else {
+                            return "#ccc";
+                        }
+                    })
+                    .style("stroke", null)
+                    .attr("stroke-width", 0.2);
+            
+                svg.transition().duration(800).call(
+                    zoomFunction.transform,
+                    d3.zoomIdentity
+                );
+            });
+            
+                
+        regionLegend.selectAll("text")
+            .data(regions)
+            .enter()
+            .append("text")
+            .attr("x", 20 +legendWidth / 2)
+            .attr("y", (d, i) => i * (legendHeight + 5) + legendHeight / 2)
+            .attr("dy", "0.35em")
+            .text(d => d)
+            .style("font-size", "15px")
+            .style("fill", "black")
+            .style("pointer-events", "none");
+        } 
     });
 })
 .catch(error => {
@@ -252,38 +394,70 @@ loadData();
 d3.select("#dataset-select").on("change", function() {
     var selectedDataset = d3.select(this).property("value");
     loadData(selectedDataset);
+    d3.select(".region-legend").remove();
 });
 // Handle year selection change
 d3.select("#year").on("change", function() {
     // Call the loadData function to update the map with the new year's data
     loadData();
+    d3.select(".region-legend").remove();
 });
+function zoomToRegion(regionName, json) {
+    const bbox = getRegionBoundingBox(regionName, json);
+    
+    // Calculate the width and height of the bounding box
+    const bboxWidth = bbox[1][0] - bbox[0][0];
+    const bboxHeight = bbox[1][1] - bbox[0][1];
+    
+    // Calculate the zoom level based on the bounding box size
+    const maxBboxSize = Math.max(bboxWidth, bboxHeight);
+    const zoomLevel = Math.min(width, height) / maxBboxSize;
+    
+    // Center of the bounding box
+    const center = [(bbox[0][0] + bbox[1][0]) / 2, (bbox[0][1] + bbox[1][1]) / 2];
 
-// Function to adjust initial zoom and centering
-function zoomToBoundingBox(bbox) {
-    const [[x0, y0], [x1, y1]] = bbox;
-    const bounds = [[x0, y0], [x1, y1]];
-
-    // Compute the center of the bounding box
-    const center = [
-        (bounds[0][0] + bounds[1][0]) / 2,
-        (bounds[0][1] + bounds[1][1]) / 2
-    ];
-
-    // Compute the zoom level based on the bounding box width
-    const dx = bounds[1][0] - bounds[0][0];
-    const dy = bounds[1][1] - bounds[0][1];
-    const zoom = Math.min(12, 0.9 / Math.max(dx / width, dy / height));
-
-    // Return the center and zoom level, but don't apply the zoom and pan to the map
-    return { center, zoom };
+    // Apply the zoom transformation
+    svg.transition().duration(800).call(
+        zoomFunction.transform,
+        d3.zoomIdentity
+            .translate(width / 2, height / 2)
+            .scale(zoomLevel)
+            .translate(-center[0], -center[1])
+    );
 }
+
+// Function to get the bounding box of a region based on its name
+function getRegionBoundingBox(regionName, geojsonData) {
+    // Initialize variables for bounding box coordinates
+    let minX = Infinity;
+    let minY = Infinity;
+    let maxX = -Infinity;
+    let maxY = -Infinity;
+
+    // Iterate over the features in the GeoJSON data
+    geojsonData.features.forEach(function(feature) {
+        // Check if the feature belongs to the specified region
+        if (feature.properties.region === regionName) {
+            // Get the bounding box of the feature
+            const bbox = path.bounds(feature);
+            // Update the bounding box coordinates if necessary
+            minX = Math.min(minX, bbox[0][0]);
+            minY = Math.min(minY, bbox[0][1]);
+            maxX = Math.max(maxX, bbox[1][0]);
+            maxY = Math.max(maxY, bbox[1][1]);
+        }
+    });
+
+    // Return the bounding box coordinates as a nested array
+    return [[minX, minY], [maxX, maxY]];
+}
+
 
 // Function to handle province click
 function provinceClicked(event, d) {
     event.stopPropagation();
     const [[x0, y0], [x1, y1]] = path.bounds(d);
-    svg.transition().duration(750).call(
+    svg.transition().duration(800).call(
         zoomFunction.transform,
         d3.zoomIdentity
             .translate(width / 2, height / 2)
@@ -295,7 +469,7 @@ function provinceClicked(event, d) {
 function handleOutsideClick(event) {
     // Check if the click target is not a province path
     if (!event.target.classList.contains("province")) {
-        svg.transition().duration(750).call(
+        svg.transition().duration(800).call(
             zoomFunction.transform,
             d3.zoomIdentity
         );
@@ -332,3 +506,23 @@ const zoomFunction = d3.zoom()
     });
 
 svg.call(zoomFunction);
+/*
+// Function to adjust initial zoom and centering
+function zoomToBoundingBox(bbox) {
+    const [[x0, y0], [x1, y1]] = bbox;
+    const bounds = [[x0, y0], [x1, y1]];
+
+    // Compute the center of the bounding box
+    const center = [
+        (bounds[0][0] + bounds[1][0]) / 2,
+        (bounds[0][1] + bounds[1][1]) / 2
+    ];
+
+    // Compute the zoom level based on the bounding box width
+    const dx = bounds[1][0] - bounds[0][0];
+    const dy = bounds[1][1] - bounds[0][1];
+    const zoom = Math.min(12, 0.9 / Math.max(dx / width, dy / height));
+
+    // Return the center and zoom level, but don't apply the zoom and pan to the map
+    return { center, zoom };
+}*/
