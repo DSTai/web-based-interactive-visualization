b315f01b522eff37e77df6c4458b73c2e70c773e
JHThao <thanhthao100402@gmail.com>
2024-05-24 19:12:53 +0700

    Update scatterplot.js

--- scatterplot.js
+++ scatterplot.js
@@ -113,7 +113,7 @@ function drawScatterPlot(data) {
     d3.select('#scatter-plot').selectAll('*').remove();
 
     // Set up the SVG container dimensions for the scatter plot
-    const margin = { top: 50, right: 20, bottom: 50, left: 50 };
+    const margin = { top: 50, right: 20, bottom: 50, left: 60 };
     const scatterWidth = 900 - margin.left - margin.right;
     const scatterHeight = 600 - margin.top - margin.bottom;
 
@@ -163,7 +163,7 @@ function drawScatterPlot(data) {
         .call(d3.axisLeft(yScale))
         .append("text")
         .attr("transform", "rotate(-90)")
-        .attr("y", -margin.left)
+        .attr("y", 10-margin.left)
         .attr("x", 50-scatterHeight / 2)
         .attr("dy", "0.71em")
         .attr("fill", "Blue")
